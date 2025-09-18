aef35a6686e6963d78f06e45f45fbb9f736f38c1
Bhargav Sriram Siddani <siddanibhargav@gmail.com>
2025-09-17 17:16:55 -0700

    Changes to spde solver to handle non-periodic boundaries.

--- exec/ML/Gaussian_RW/flow_matching/particle_simulator/spde_solver.py
+++ exec/ML/Gaussian_RW/flow_matching/particle_simulator/spde_solver.py
@@ -18,6 +18,7 @@ from random_walkers_pytorch import get_particle_positions
 from random_walkers_pytorch import get_well_initial_pos
 from random_walkers_pytorch import get_uni_initial_pos
 from random_walkers_pytorch import get_density
+from random_walkers_pytorch import boundary_asserts
 #######################################################
 
 torch.set_default_device('cpu')
@@ -26,7 +27,7 @@ torch.set_default_device('cpu')
 @hydra.main(version_base=None, config_path="./conf",
             config_name="config_spde")
 def fhd_model_run (cfg):
-    dataset_name = cfg.dataset_name
+    dataset_name = cfg.dataset.name
     assert dataset_name in ["uniform", "well", "reservoir"]
     print("Creating "+dataset_name+" dataset")
 
@@ -51,8 +52,8 @@ def fhd_model_run (cfg):
                                            x_1=0.25, x_2=0.75,
                                            len_system=len_system)
     elif dataset_name == "reservoir":
-        left_boundary  = ["put", 20]
-        right_boundary = ["ignore", 0]
+        left_boundary  = list(cfg.dataset.left_boundary)
+        right_boundary = list(cfg.dataset.right_boundary)
         initial_pos = get_uni_initial_pos(ncells,par_per_cell,
                                           len_system=len_system)
     else:
@@ -63,54 +64,94 @@ def fhd_model_run (cfg):
     par_per_cell /= ncells
 
     cell_centers = torch.linspace(0.5*dx,len_system-0.5*dx,ncells)
-    left_boundary  = ["periodic", 0]
-    right_boundary = ["periodic", 0]
+    periodic_boundary = boundary_asserts(left_boundary, right_boundary)
 
     n_total_steps = cfg.n_total_particle_steps
     n_samples   = cfg.n_samples
 
-    # Evolve the initial position to remove transients
-    initial_pos = random_walk_just_evolve(ncells, cfg.n_thermalize_steps,
-                                          dt, initial_pos.clone(),
-                                          left_boundary, right_boundary,
-                                          len_system = len_system)
-
-    initial_density = get_density(cell_centers,
-                                  initial_pos.clone()).float()
-    initial_density /= dx
+    # gauss_data for SPDE, grnd_trth_data for particle simulator
     gauss_data     = torch.zeros((n_samples,n_total_steps+1,ncells))
-    gauss_data[:,0, :] = initial_density.unsqueeze(0).repeat(n_samples,1)
+    grnd_trth_data  = torch.zeros((n_samples,n_total_steps+1,ncells))
 
-    dens_old = initial_density.clone()
+    # Same initial condition for all realizations in an ensemble
+    same_ic = cfg.same_ic
+    n_thermalize_steps = cfg.n_thermalize_steps
+
+    if same_ic:
+        # Evolve the initial position to remove transients
+        initial_pos = random_walk_just_evolve(ncells, n_thermalize_steps,
+                                              dt, initial_pos.clone(),
+                                              left_boundary, right_boundary,
+                                              len_system = len_system)
+
+    dens_old = torch.zeros((ncells,))
     dens_new = torch.zeros_like(dens_old)
     left_dens = torch.zeros_like(dens_old)
     right_dens = torch.zeros_like(dens_old)
 
     for i_ens in range(n_samples):
-        dens_old[...] = initial_density[...]
+        if (not same_ic):
+            # Evolve the initial position to remove transients
+            initial_pos = random_walk_just_evolve(ncells, n_thermalize_steps,
+                                                  dt, initial_pos.clone(),
+                                                  left_boundary, right_boundary,
+                                                  len_system = len_system)
+        ######### SPDE ###################################################
+        initial_density_spde = get_density(cell_centers,
+                                      initial_pos.clone()).float()
+        initial_density_spde /= dx
+        gauss_data[i_ens,0, :] = initial_density_spde[:]
+        dens_old[...] = initial_density_spde[...]
+        ###################################################################
+        ####### Particle simulation #######################################
+        initial_density_ps = get_density(cell_centers, initial_pos.clone())
+        grnd_trth_data[i_ens,0, :] = initial_density_ps[:]
+        iter_pos = initial_pos.clone()
+        ###################################################################
         for i_t in range(1,n_total_steps+1):
+            #### SPDE #########################
             right_dens[:-1] = dens_old[1:]
-            right_dens[-1] = dens_old[0]
-
+            ##### Boundary effects ###################
+            if periodic_boundary:
+                right_dens[-1] = dens_old[0]
+            else:
+                if right_boundary[1] > 0:
+                    right_dens[-1] = (np.random.poisson(
+                                         float(right_boundary[1])))/dx
+                else:
+                    right_dens[-1] = 0.
+            #############################################
             left_dens[1:] = dens_old[:-1]
-            left_dens[0]  = dens_old[-1]
-
+            ################ Periodic effects ##############
+            if periodic_boundary:
+                left_dens[0]  = dens_old[-1]
+            else:
+                if left_boundary[1] > 0:
+                    left_dens[0] = (np.random.poisson(
+                                       float(left_boundary[1])))/dx
+                else:
+                    left_dens[0] = 0.
+            ##################################################
             flux_mean_p = (0.5/dx)*(right_dens-dens_old)
             flux_mean_m = (0.5/dx)*(dens_old-left_dens)
-
             # Noise corresponding to faces
-            noise = torch.randn_like(dens_old)
-            noise_periodic = torch.cat([noise, noise[0:1]])
-
             flux_fluc_p = 0.5*(torch.sqrt(torch.clamp(right_dens,min=0.))+
                                torch.sqrt(torch.clamp(dens_old,min=0.)))
             flux_fluc_p *= np.sqrt(1/(dt*dx))
-            flux_fluc_p *= noise_periodic[1:]
 
             flux_fluc_m = 0.5*(torch.sqrt(torch.clamp(dens_old,min=0.))+
                                torch.sqrt(torch.clamp(left_dens,min=0.)))
             flux_fluc_m *= np.sqrt(1/(dt*dx))
-            flux_fluc_m *= noise_periodic[:-1]
+
+            if periodic_boundary:
+                noise = torch.randn_like(dens_old)
+                noise_periodic = torch.cat([noise, noise[0:1]])
+                flux_fluc_p *= noise_periodic[1:]
+                flux_fluc_m *= noise_periodic[:-1]
+            else:
+                noise = torch.randn((ncells+1,))
+                flux_fluc_p *= noise[1:]
+                flux_fluc_m *= noise[:-1]
 
             dens_new = (flux_mean_p+flux_fluc_p) - (flux_mean_m+flux_fluc_m)
             dens_new /= dx
@@ -118,19 +159,18 @@ def fhd_model_run (cfg):
             dens_new += dens_old
             gauss_data[i_ens,i_t,:] = dens_new
             dens_old[...] = dens_new[...]
-
-    # Particle-based solver
-    initial_density = get_density(cell_centers, initial_pos.clone())
-    grnd_trth_data  = torch.zeros((n_samples,n_total_steps+1,ncells))
-    grnd_trth_data[:,0, :] = initial_density.unsqueeze(0).repeat(n_samples,1)
-    for iter_val in range(n_samples):
-        iter_pos = initial_pos.clone()
-        for i_t in range(n_total_steps):
+            ###############################################################
+            ###### Particle Simulation ###################################
             iter_pos, new_density, _ = random_walk_v2(ncells, 1, dt,
                                            iter_pos.clone(),
                                            left_boundary, right_boundary,
                                            len_system = len_system)
-            grnd_trth_data[iter_val,i_t+1,:] =  new_density[...]
+
+            grnd_trth_data[i_ens,i_t,:] =  new_density[:]
+            ###############################################################
+
+        if (i_ens % 10 == 0):
+            print(f"{i_ens+1} realizations completed.")
 
     grnd_trth_np   = grnd_trth_data.cpu().numpy()
     gauss_data_np    = gauss_data.cpu().numpy()
