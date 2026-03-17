7364e42d7a6562e54d05664790dd1b84b4288240
Bhargav Sriram Siddani <siddanibhargav@gmail.com>
2026-03-16 18:58:43 -0700

    Scripted model

---
+++ exec/ML/Gaussian_RW/flow_matching/ML_runs/run_17/script_model.py
@@ -0,0 +1,43 @@
+import torch
+from model_reflect import Flow_Transformer
+
+torch.autograd.set_grad_enabled(False)
+
+flow = Flow_Transformer(input_N_dim=2, input_F_dim=1,
+                        d_model = 256, nhead=4,
+                        num_encoder_layers=2,
+                        num_decoder_layers=2,
+                        dropout = 0.,
+                        max_len=50, d_embed=256,
+                        n_layers=2,act_func=torch.nn.ReLU(),
+                        residual_con=True)
+
+model_folder = "./outputs/2026-03-03/18-02-59/"
+
+chpt_fl = torch.load(model_folder +
+                   "flow_transformer_model_2026-03-04_00-27-20.tar",
+                   weights_only=False)
+flow.load_state_dict(chpt_fl['model_state_dict'])
+# Dropout needs to be turned off for symmetry
+flow.train(False)
+
+scripted_flow = torch.jit.script(flow)
+
+input_N_dim = 2
+input_F_dim = 1
+seq_len = 10
+batch_sz = 10
+x_N = torch.randn(batch_sz, seq_len,input_N_dim)
+x_F = torch.randn(batch_sz, seq_len-1,input_F_dim)
+x_t = torch.randn(batch_sz, 1, 1)
+t = torch.randn(batch_sz, 1, 1)
+
+y_1 = flow(x_t, x_N, x_F, t)
+print(y_1)
+
+y_2 = scripted_flow(x_t, x_N, x_F, t)
+print(y_2)
+
+assert torch.allclose(y_1,y_2)
+
+torch.jit.save(scripted_flow, model_folder+"scripted_model.pt")
